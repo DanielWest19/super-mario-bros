428d6272c8cc4616cdc2f8a16b490b318c1201bf
daniel west <danielwestdesouza20@gmail.com>
2023-01-30 13:39:25 -0300

    add new ordens in js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const butonTrailer = document.querySelector(".buton-trailer")
 const modal = document.querySelector(".modal")
 const fecharModal = document.querySelector(".fechar-modal")
+const video = document.getElementById("trailer")
+const linkDoVideo = trailer.src
 
 butonTrailer.addEventListener("click", () => {
-  modal.classList.add("aberto")
-})
+  modal.classList.toggle("aberto")
+  video.setAttribute("src", linkDoVideo)
+});
+
+fecharModal.addEventListener("click", () => {
+  modal.classList.toggle("aberto");
+  video.setAttribute("src", "")
+});
